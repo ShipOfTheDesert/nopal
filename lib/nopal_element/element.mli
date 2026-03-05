c89f8cf171119dd5bcc55b9885344ca4aad48f71
Miguel Lopez V <miguellova@gmail.com>
2026-03-05 02:40:47 -0600

    chore: update doc

--- lib/nopal_element/element.mli
+++ lib/nopal_element/element.mli
@@ -1,7 +1,7 @@
 (** A pure description of a UI element.
 
-    This is a stub type for the PoC. The full element DSL will be defined in a
-    future PRD/RFC. *)
+    This is a stub type for the PoC. The full element DSL will be defined in the
+    future. *)
 
 type 'msg t =
   | Text of string  (** A text node with the given content. *)
